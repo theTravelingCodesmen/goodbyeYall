fcd7df0da0492f4d7badbaf3910f614d44af91bd
owentemp <owentemple@gmail.com>
2016-06-10 14:35:44 -0500

    Set up schema template

--- db/db.js
+++ db/db.js
@@ -1,1 +1,107 @@
 //
+// db.js
+//
+// KNEX database library
+// plus additional database helper functions
+//
+var path = require('path');
+
+var config = require('./knexfile')
+var env = process.env.NODE_ENV || 'development'
+var knex = require('knex')(config[env])
+var _ = require('underscore');
+
+module.exports = knex;
+
+knex.ensureSchema = ensureSchema = function () {
+  return Promise.all([
+    knex.schema.hasTable('destinations').then(function (exists) {
+      if (!exists) {
+        knex.schema.createTable('destinations', function (table) {
+          table.increments('id').primary();
+          table.string('title', 255);
+          table.string('airport_code', 255);
+          table.string('city_name', 255);
+          table.string('main_image_url');
+          table.string('next_image_url', 255);
+          table.string('intro', 255);
+          table.string('country', 255);
+        }).then(function (table) {
+          console.log('Created destinations table.');
+        })
+      }
+    }),
+
+    knex.schema.hasTable('quotes').then(function (exists) {
+      if (!exists) {
+        knex.schema.createTable('quotes', function (table) {
+          table.increments('quote_id').primary();
+
+
+          table.string('trail_name', 255);
+          table.integer('max_eq_length');
+          table.integer('max_people');
+          table.integer('campground_id_fk');
+          table.integer('site_id');
+          table.string('site_name', 255);
+          table.string('site_type', 255);
+          table.string('waterfront', 255);
+          table.boolean('water');
+          table.boolean('amps');
+          table.boolean('pets');
+          table.boolean('sewer');
+          // table.timestamps();
+        }).then(function (table) {
+          console.log('Created campsites table.');
+        });
+      }
+    })
+  ])
+}
+
+knex.deleteEverything = function () {
+  return knex('campsites').truncate()
+    .then(function () {
+      return knex('campgrounds').truncate()
+    })
+    .then(function () {
+      console.log("Deleted campgrounds and campsites db tables")
+    })
+}
+
+//
+// Select all campgrounds from the campgrounds table
+//
+knex.queryCampgrounds = function() {
+  return knex('campgrounds').select();
+};
+
+//
+// Select all campsites with the given campground ID
+//
+knex.queryCampsites = function(cg_id) {
+  return knex('campsites').where({'campground_id_fk': cg_id}).select();
+}
+
+//
+// Insert all elements of a campground/campsite array into the given table name
+//
+knex.insertEverything = function(campArr, table) {
+  return Promise.all(_.map(campArr, function(camp) {
+    return knex(table).insert(camp)
+      .then(function (res) {
+        console.log("Added entry to " + table + " table: ", res);
+      })
+      .catch(function (err) {
+        console.log("Error inserting into " + table + " table: ", err);
+      })
+  })).then(function() {
+    return campArr;
+  });
+}
+
+knex.closeDb = function () {
+  knex.destroy().then(function () {
+    console.log("Closed db connection")
+  })
+}
--- db/knex.js
+++ db/knex.js
@@ -1,1 +1,32 @@
+// knexfile.js
 //
+// Configures KNEX library to use SQLITE3 adapter and
+// filepath of local database
+//
+//
+var path = require('path');
+
+module.exports = {
+
+// TODO: Configure KNEX for SQLITE3 db
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.join(__dirname, '/cgdb_dev.sqlite')
+    }
+  },
+
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.join(__dirname, '/cgdb_test.sqlite')
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
+};
+
