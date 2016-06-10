d29457de82f2c5b2c1a659fa92164ce0a004782f
pathtofreedom <PathToFreedom@Justins-MacBook-Air.local>
2016-06-10 14:43:57 -0500

    reconfig server.js for testing routes

--- server/app.js
+++ server/app.js
@@ -6,21 +6,26 @@ let logger = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
 
-
-let app = express();
-
-// uncomment after favicon moved into public
-// app.use(favicon(path.join(__dirname, 'client', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
-
-app.use('/hello-traveling-codesman', function(req, res){
+let routes = express.Router();
+routes.use(express.static(path.join(__dirname, '..', 'client', 'public')));
+routes.use('/hello-traveling-codesman', function(req, res){
 	res.end('hello travelingcodesman!');
 })
-// at api endpoint here when available
+if(process.env.NODE_ENV === 'test'){
+	module.exports = routes;
+} else {
+		let app = express();
 
-app.listen(process.env.PORT||4000);
+		// uncomment after favicon moved into public
+		// app.use(favicon(path.join(__dirname, 'client', 'favicon.ico')));
+		app.use(logger('dev'));
+		app.use(bodyParser.json());
+		app.use(bodyParser.urlencoded({ extended: false }));
+		app.use(cookieParser());
+		app.use('/', routes);
+
+		// at api endpoint here when available
+
+		app.listen(process.env.PORT||4000);
+	
+	};
