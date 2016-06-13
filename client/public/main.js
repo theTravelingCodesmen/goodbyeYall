90b77e9e85f61a1398629aaa5377e2046a8566f2
James <james.mark.fitzpatrick@gmail.com>
2016-06-13 12:26:58 -0500

    adds flightData component; adds reference in static HTML

--- client/public/main.js
+++ client/public/main.js
@@ -8,6 +8,7 @@ import PhotoFour from '../components/photoSlotFour'
 import PhotoFive from '../components/photoSlotFive'
 import PhotoSix from '../components/photoSlotSix'
 import Intro from '../components/intro'
+import FlightData from '../components/flightData'
 
 
 class Test extends React.Component {
