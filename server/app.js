2dde6718ac1d664c0163a539d5233260e18a2979
Sin Cheung Ko <scko823@gmail.com>
2016-06-13 12:20:50 -0500

    refactor call to skyscanner to skyscanner_api router in the server

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ let favicon = require('serve-favicon');
 let logger = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
-let requestpromise = require('request-promise');
+let skyscanner_api = require('./apis/skyscanner_api')
 
 let routes = express.Router();
 routes.use(express.static(path.join(__dirname, '..', 'client', 'public')));
@@ -26,22 +26,7 @@ if(process.env.NODE_ENV === 'test'){
 		app.use(bodyParser.urlencoded({ extended: false }));
 		app.use(cookieParser());
 		app.use('/', routes);
-		app.use('/skyscanner_api/d3_cache/:location/:currency/:ISO/:dest/:arrival/:depart_time/:arrival_time', function(req, res){
-			let skyscanner_endpoint = 'http://partners.api.skyscanner.net/apiservices/browsedates/v1.0';
-			skyscanner_endpoint+= '/' + req.params.location;
-			skyscanner_endpoint+= '/' + req.params.currency;
-			skyscanner_endpoint+= '/' + req.params.ISO;
-			skyscanner_endpoint+= '/' + req.params.dest;
-			skyscanner_endpoint+= '/' + req.params.arrival;
-			skyscanner_endpoint+= '/' + req.params.depart_time;
-			skyscanner_endpoint+= '/' + req.params.arrival_time;
-			skyscanner_endpoint+= '?apiKey=' + req.query.apiKey;
-			requestpromise(skyscanner_endpoint).then(JSON.parse).then(function(resp){
-				res.json(resp)
-			})
-			
-		});
-
+		app.use('/skyscanner_api', skyscanner_api);
 		// at api endpoint here when available
 
 		app.listen(process.env.PORT||4000);
