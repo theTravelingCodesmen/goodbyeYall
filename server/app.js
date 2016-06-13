9a4bd01bb855d32e9cdb0b95ccfc26e618761b72
Sin Cheung Ko <scko823@gmail.com>
2016-06-13 12:08:15 -0500

    prepare server to get api call from client for price cache data for d3

--- server/app.js
+++ server/app.js
@@ -11,6 +11,8 @@ routes.use(express.static(path.join(__dirname, '..', 'client', 'public')));
 routes.use('/hello-traveling-codesman', function(req, res){
 	res.end('hello travelingcodesman!');
 })
+
+
 if(process.env.NODE_ENV === 'test'){
 	module.exports = routes;
 } else {
@@ -23,6 +25,18 @@ if(process.env.NODE_ENV === 'test'){
 		app.use(bodyParser.urlencoded({ extended: false }));
 		app.use(cookieParser());
 		app.use('/', routes);
+		app.use('/skyscanner_api/d3_cache/:location/:currency/:ISO/:dest/:arrival/:depart_time/:arrival_time', function(req, res){
+			let skyscanner_endpoint = 'http://partners.api.skyscanner.net/apiservices/browsedates/v1.0';
+			skyscanner_endpoint+= '/' + req.params.location;
+			skyscanner_endpoint+= '/' + req.params.currency;
+			skyscanner_endpoint+= '/' + req.params.ISO;
+			skyscanner_endpoint+= '/' + req.params.dest;
+			skyscanner_endpoint+= '/' + req.params.arrival;
+			skyscanner_endpoint+= '/' + req.params.depart_time;
+			skyscanner_endpoint+= '/' + req.params.arrival_time;
+			skyscanner_endpoint+= '?apiKey=' + req.query.apiKey;
+			res.json({'realurl': skyscanner_endpoint})
+		});
 
 		// at api endpoint here when available
 
