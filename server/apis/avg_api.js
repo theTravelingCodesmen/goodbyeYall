342cc3e5ea5058c63a65870dfa4a6510e575b905
Sin Cheung Ko <scko823@gmail.com>
2016-06-15 16:05:47 -0500

    clean up /avg_api endpoint file

--- server/apis/avg_api.js
+++ server/apis/avg_api.js
@@ -4,7 +4,7 @@ let router = require('express').Router();
 let knex = require('../../db/db');
 
 
-knex.getAvg = function(originCity, destCity){
+knex.getAvgByRoute = function(originCity, destCity){
 	return knex('averages').where('avg_price','>',1).where({
 		originCity:originCity,
 		destinationCity:destCity
@@ -15,7 +15,7 @@ module.exports = router;
 
 
 router.get('/:originCity/:destCity', function(req, res){
-	knex.getAvg(req.params.originCity,req.params.destCity)
+	knex.getAvgByRoute(req.params.originCity,req.params.destCity)
 		.then((array)=>array.map(price=>{ 
 			let date = price.outboundYear+'-'+price.outboundMonth
 			return {
@@ -27,20 +27,3 @@ router.get('/:originCity/:destCity', function(req, res){
 
 });
 
-
-
-// router.get('/d3_cache/:location/:currency/:ISO/:dest/:arrival/:depart_time/:arrival_time', function(req, res){
-// 	let skyscanner_endpoint = 'http://partners.api.skyscanner.net/apiservices/browsedates/v1.0';
-// 	skyscanner_endpoint+= '/' + req.params.location;
-// 	skyscanner_endpoint+= '/' + req.params.currency;
-// 	skyscanner_endpoint+= '/' + req.params.ISO;
-// 	skyscanner_endpoint+= '/' + req.params.dest;
-// 	skyscanner_endpoint+= '/' + req.params.arrival;
-// 	skyscanner_endpoint+= '/' + req.params.depart_time;
-// 	skyscanner_endpoint+= '/' + req.params.arrival_time;
-// 	skyscanner_endpoint+= '?apiKey=' + req.query.apiKey;
-// 	requestpromise(skyscanner_endpoint).then(JSON.parse).then(function(resp){
-// 		res.json(resp)
-// 	})
-	
-// });
