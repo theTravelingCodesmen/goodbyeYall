e485a1a9a4639176ff0fb1c3f1456af2db8c305f
owentemp <owentemple@gmail.com>
2016-06-15 11:57:13 -0500

    style font and card title elements

--- client/components/intro.js
+++ client/components/intro.js
@@ -6,7 +6,7 @@ import ReactDom from 'react-dom'
 class Intro extends React.Component{
 	render(){
 		return (
-			<div className={this.props.className}>placeholder for database entry</div>
+			<div className={this.props.className}>The Great Wall of China was built to link existing fortifications into a united defense system and better keep invading Mongol tribes out of China. It is the largest man-made monument ever to have been built and it is disputed that it is the only one visible from space. Many thousands of people must have given their lives to build this colossal construction.</div>
 		)
 	}
 }
