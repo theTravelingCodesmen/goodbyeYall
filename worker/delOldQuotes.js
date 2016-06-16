43d03abc3aac4322e2fd26ef3c4fdde6270df3e1
Sin Cheung Ko <scko823@gmail.com>
2016-06-16 11:10:21 -0500

    finish worker function to delete rows that are calculated and more than 36 hours old

--- worker/delOldQuotes.js
+++ worker/delOldQuotes.js
@@ -3,13 +3,20 @@
 let knex = require('../db/db');
 
 
-knex.getOldQuotes = function(){
+knex.delOldQuotes = function(){
 	//get any quotes in the quote tables
 	//console.log('datetime for 36 hours ago: ', Date.now()-36*60*60*1000)
 	return knex('quotes')
 				.where('created_at','<', new Date(Date.now()-36*60*60*1000))
 				.where('calculated', true)
+				.del()
 }
 
 
-knex.getOldQuotes().then(function(data){console.log(data)})
+knex.delOldQuotes()
+	.then(function(data){console.log("Total number of rows deleted: ", data)})
+	.then(function(){
+		console.log('==============================');
+		console.log('deleted all quotes that are calculated and older than 36 hours old')
+	})
+	.then(knex.closeDb)
