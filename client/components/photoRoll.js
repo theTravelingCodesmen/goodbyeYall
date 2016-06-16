16f7bbe8542fc3a310ce71784acac29943147938
Sin Cheung Ko <scko823@gmail.com>
2016-06-16 12:14:30 -0500

    clean up .photo-roll by making parent container .row

--- client/components/photoRoll.js
+++ client/components/photoRoll.js
@@ -12,14 +12,14 @@ import PhotoSix from '../components/photoSlotSix'
 class PhotoRoll extends React.Component {
 	render() {
 		return(
-			<div>
-				<ul className='photo-roll'>
-					<PhotoOne className={'chichen col-md-2 col-sm-10' }  src={'chichen_itza.png'} />
-					<PhotoTwo className={'christ col-md-2 col-sm-10'} src={'christ_the_redeemer.png'} />
-					<PhotoThree className={'colusseum col-md-2 col-sm-10'} src={'colusseum.png'} />
-					<PhotoFour className={'machu col-md-2 col-sm-10'} src={'machu_picchu.png'} />
-					<PhotoFive className={'petra col-md-2 col-sm-10'} src={'petra.png'} />
-					<PhotoSix className={'taj col-md-2 col-sm-10'} src={'taj_mahal.png'} />
+			<div className='row'>
+				<ul className='photo-roll' >
+					<PhotoOne className='chichen col-md-2 col-sm-10'   src='chichen_itza.png' />
+					<PhotoTwo className='christ col-md-2 col-sm-10' src='christ_the_redeemer.png' />
+					<PhotoThree className='colusseum col-md-2 col-sm-10' src='colusseum.png' />
+					<PhotoFour className='machu col-md-2 col-sm-10' src='machu_picchu.png' />
+					<PhotoFive className='petra col-md-2 col-sm-10' src='petra.png' />
+					<PhotoSix className='taj col-md-2 col-sm-10' src='taj_mahal.png' />
 				</ul>
 			</div>
 		)
