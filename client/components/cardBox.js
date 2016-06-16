68518e4863c6968cde6a70d0ed9f7800050723a9
James <james.mark.fitzpatrick@gmail.com>
2016-06-16 16:41:38 -0500

    adds props to componets in CardBox

--- client/components/cardBox.js
+++ client/components/cardBox.js
@@ -27,6 +27,16 @@ class CardBox extends React.Component {
 		const destinationsList = [
 			{id:1, title: 'Colosseum', airport_code: 'ROME-sky', country:'Italy', 
 			city_name: 'Rome', intro: "a", package_group: 'Seven Wonders of the World',
+			main_image_url: '/assets/images/petra.png', next_image_url: 
+			'/assets/images/petra.jpeg'
+	   },
+	   {id:2, title: 'Colosseum', airport_code: 'ROME-sky', country:'Italy', 
+			city_name: 'Rome', intro: "a", package_group: 'Seven Wonders of the World',
+			main_image_url: '/assets/images/colusseum.png', next_image_url: 
+			'/assets/images/colusseum.jpeg'
+	   },
+	   			{id:1, title: 'Colosseum', airport_code: 'ROME-sky', country:'Italy', 
+			city_name: 'Rome', intro: "a", package_group: 'Seven Wonders of the World',
 			main_image_url: '/assets/images/colusseum.png', next_image_url: 
 			'/assets/images/colusseum.jpeg'
 	   },
@@ -34,14 +44,51 @@ class CardBox extends React.Component {
 			city_name: 'Rome', intro: "a", package_group: 'Seven Wonders of the World',
 			main_image_url: '/assets/images/colusseum.png', next_image_url: 
 			'/assets/images/colusseum.jpeg'
-	   }]
+	   },
+	   			{id:1, title: 'Colosseum', airport_code: 'ROME-sky', country:'Italy', 
+			city_name: 'Rome', intro: "a", package_group: 'Seven Wonders of the World',
+			main_image_url: '/assets/images/colusseum.png', next_image_url: 
+			'/assets/images/colusseum.jpeg'
+	   },
+	   {id:2, title: 'Colosseum', airport_code: 'ROME-sky', country:'Italy', 
+			city_name: 'Rome', intro: "a", package_group: 'Seven Wonders of the World',
+			main_image_url: '/assets/images/colusseum.png', next_image_url: 
+			'/assets/images/colusseum.jpeg'
+	   },
+	   			{id:1, title: 'Colosseum', airport_code: 'ROME-sky', country:'Italy', 
+			city_name: 'Rome', intro: "a", package_group: 'Seven Wonders of the World',
+			main_image_url: '/assets/images/colusseum.png', next_image_url: 
+			'/assets/images/colusseum.jpeg'
+	   }
+	  ]
 
 		return destinationsList.map((obj,index) => {
 			if(index === 0){
-				obj.currentlyActivePhoto = true
+				obj.currentlyActivePhoto = true;
+				//rename img urls 
+				return <ActivePhoto 
+					title={obj.title} 
+					airport_code={obj.airport_code} 
+					country={obj.country} 
+					city_name={obj.city_name} 
+					intro={obj.intro} 
+					package_group={obj.package_group} 
+					main_image_url={obj.main_image_url} 
+					next_image_url={obj.next_image_url}
+				/>
 			}
 			else{
-				obj.currentlyActivePhoto = false            
+				obj.currentlyActivePhoto = false;
+				return <PassivePhoto 
+					title={obj.title} 
+					airport_code={obj.airport_code} 
+					country={obj.country} 
+					city_name={obj.city_name} 
+					intro={obj.intro} 
+					package_group={obj.package_group} 
+					main_image_url={obj.main_image_url} 
+					next_image_url={obj.next_image_url}
+				/>
 			}
 		})
 	}
