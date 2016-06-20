ee9485374188d4291444da41c3ddac63b4f8559a
Paul-Michael Schreiber <paulmichael.schreiber@gmail.com>
2016-06-20 17:41:52 -0500

    finish reducing the data

--- server/apis/packages.js
+++ server/apis/packages.js
@@ -11,24 +11,11 @@ knex.getCheapestByRoute = function (originCity, destinationCity) {
 }
 
 
-
 knex.getBookNowDetails = function (originCity, destinationCity) {
 	return knex('quotes').where({
 		originCity:originCity,
 		destinationCity:destinationCity
 	}).select('price', 'deepLink', 'outboundDate', 'inboundDate', 'originCity', 'destinationCity', 'created_at')
-		.then((arr) => {
-			return arr.reduce( (prev, curr) => {
-				return prev.price < curr.price ? prev : curr
-			})
-		})
-		.then( (x) => {
-			console.log(x)
-			return x
-		})
-		.catch((err) => { 
-			console.log(err)
-		})
 }
 
 
@@ -43,14 +30,17 @@ knex.getCheapestInLastThirtyDays = function (originCity, destinationCity) {
 router.use('/selectpackage/:packagename', function(req, res){
   knex('destinations').where({'package_group': req.params.packagename}).select('*')
   	.then(function(data){
-  		return Promise.all(data.map( (destination) => {
-  		  			//remove hard coding of HOUA-sky
-  		  			return knex.getBookNowDetails('HOUA-sky', destination.airport_code)
-  		  			.then( (data) => {
-  		  				destination.bookDetails = data;
-  		  				return destination
-  		  			})
-  		  		}))
+			return Promise.all(
+				data.map( (destination) => {
+					//remove hard coding of HOUA-sky
+					return knex.getBookNowDetails('HOUA-sky', destination.airport_code)
+				.then( (info) => {
+					destination.bookDetails = info.reduce( (prev, curr) => {
+					return prev.price < curr.price ? prev : curr
+				}, {});
+					return destination
+				})
+			}))
   	})
   	.then( (data) => {
   		res.send(data)
