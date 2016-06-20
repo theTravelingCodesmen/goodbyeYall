45e80d24c77ea7e29f04b35f286bd08865a0b635
Sin Cheung Ko <scko823@gmail.com>
2016-06-20 12:32:22 -0500

    pseduo code how to calculate and update the cheapest route table

---
+++ worker/cheapestRouteEver.js
@@ -0,0 +1,8 @@
+// calculate the cheapest ever price by route 
+
+// first go thru the quotes dt, and grab the cheapest price by route, ignore outbound and inbound date
+
+// in the array got back, do a .map (function(route))
+	// get the origin and dest city from the dt (cheapest_route_ever)
+	// compare the two obj
+	// if the new price is cheaper, update the cheapest_route_ever row (by id)
