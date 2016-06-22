53c0629f5c153dc9e503a82a662fc2a6c3ea0f4f
Sin Cheung Ko <scko823@gmail.com>
2016-06-22 15:30:05 -0500

    wire up basic routing in nav bar

--- client/components/navBar.js
+++ client/components/navBar.js
@@ -2,31 +2,30 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem }from 'react-bootstrap';
-
+import { Link } from 'react-router';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap'
 
 class NavBar extends React.Component{
   render(){
-
     return (
     <div className="container">
-    <Navbar className="navbar-fixed-top">
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a href="#">GoodByeYall </a>
-        </Navbar.Brand>
-      </Navbar.Header>
-      <Nav>
-        <NavItem eventKey={1}>Home</NavItem>
-        <NavItem eventKey={2} href="#">How it Works</NavItem>
-        <NavItem eventKey={3} href="#">Meet the Devs</NavItem>
-        <NavDropdown eventKey={3} title="Travel Packages" id="basic-nav-dropdown">
-          <MenuItem eventKey={3.1}>Seven Wonders of the World</MenuItem>
-          <MenuItem eventKey={3.2}>Seven Natural Wonders of the World</MenuItem>
-        </NavDropdown>
-      </Nav>
-    </Navbar>
-    {this.props.children}
+      <Navbar className="navbar-fixed-top">
+        <Navbar.Header>
+          <Navbar.Brand>
+            <LinkContainer to={{pathname:'/'}}><div>GoodByeYall </div></LinkContainer>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <LinkContainer to={{pathname:'/how'}}><NavItem eventKey={2} href="#">How it Works</NavItem></LinkContainer>
+          <LinkContainer to={{pathname:'/meetthedev'}}><NavItem eventKey={3} href="#">Meet the Devs</NavItem></LinkContainer>
+          <NavDropdown eventKey={3} title="Travel Packages" id="basic-nav-dropdown">
+            <MenuItem eventKey={3.1}><LinkContainer to={{pathname:'/package/seven-wonder'}}><div>Seven Wonders of the World</div></LinkContainer></MenuItem>
+            <MenuItem eventKey={3.2}><LinkContainer to={{pathname:'/package/seven-natural-wonder'}}><div>Seven Natural Wonders of the World</div></LinkContainer></MenuItem>
+          </NavDropdown>
+        </Nav>
+      </Navbar>
+      {this.props.children}
     </div>
 
 
