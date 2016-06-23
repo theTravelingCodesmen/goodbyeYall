e8b5f356b5a681335e5b153fe57b9eca26f0f99a
Sin Cheung Ko <scko823@gmail.com>
2016-06-23 15:24:56 -0500

    add new nav dropdown menu for selecting originairport in localstorage

--- client/components/navBar.js
+++ client/components/navBar.js
@@ -10,6 +10,10 @@ class NavBar extends React.Component{
   componentWillMount(){
     localStorage.setItem('originairport', (localStorage.getItem('originairport')|| "HOUA-sky"));
   }
+  handleSelect(airport) {
+    localStorage.setItem('originairport', airport);
+    // alert(`selected ${airport}`);
+  }
   render(){
     return (
     <div className="container">
@@ -25,6 +29,10 @@ class NavBar extends React.Component{
           <NavDropdown className="navbar-button" eventKey={3} title="Travel Packages" id="basic-nav-dropdown">
             <MenuItem eventKey={3.1}><LinkContainer to={{pathname:'/package/Seven Wonders'}} className="navbar-button"><div>Seven Wonders</div></LinkContainer></MenuItem>
             <MenuItem eventKey={3.2}><LinkContainer to={{pathname:'/package/Seven Natural Wonders'}} className="navbar-button"><div>Seven Natural Wonders</div></LinkContainer></MenuItem>
+          </NavDropdown>
+          <NavDropdown className="navbar-button" eventKey={4} title="Set your airport" id="basic-nav-dropdown">
+            <MenuItem eventKey={4.1} onClick={this.handleSelect.bind(null, "DFWA-sky")}><div className="navbar-button">DFWA-sky</div></MenuItem>
+            <MenuItem eventKey={4.2} onClick={this.handleSelect.bind(null, "HOUA-sky")}><div className="navbar-button">HOUA-sky</div></MenuItem>
           </NavDropdown>
         </Nav>
       </Navbar>
