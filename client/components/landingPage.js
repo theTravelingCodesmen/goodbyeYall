930832c55840b8296422a4cc211d2f285d3238a6
Sin Cheung Ko <scko823@gmail.com>
2016-06-23 11:16:44 -0500

    landingPage Component retired

--- client/components/landingPage.js
+++ client/components/landingPage.js
@@ -1,31 +1,42 @@
-'use strict'
+// 'use strict'
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import NavBar from './navBar';
-import OuterCardBox from './outerCardBox';
 // import MissionStatement from './missionStatement'
 // import MeetTheDevs from './meetTheDevs'
-import { Route, IndexRoute, browserHistory } from 'react-router';
+// import React from 'react';
+// import ReactDOM from 'react-dom'
+// import NavBar from './navBar'
+// import OuterCardBox from './outerCardBox'
+// import MissionStatement from './missionStatement'
+// import MeetTheDevs from './meetTheDevs'
+// import { Route, IndexRoute, browserHistory } from 'react-router'
 // import routes from '../routes'
 // import CardBox from './cardBox'
 
 
-function LandingPage(){
-		return(
 
-				<Route path="/" component={NavBar}>
-					<IndexRoute component={OuterCardBox} / >
-				</Route>
-		)
-	}
+
+// function LandingPage(){
+// 		return(
+// 					// <div> 
+// 					// 	<NavBar />
+// 					// 	<div className='nav-spacing'></div>
+// 					// 	<Router history={browserHistory} routes={routes} />
+// 					// 	{this.props.children}
+// 					// </div>
+// 				<Route path="/" component={NavBar}>
+// 					<IndexRoute component={OuterCardBox} / >
+
+// 				</Route>
+// 		)
+// 	}
+
 
 
 // switch render function to here from CardBox
 
 
 
-export default LandingPage;
+// export default LandingPage;
 
 
 
