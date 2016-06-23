d28ed6dba0c0cac18eb7aea5fa027141e7ab9fbd
owentemp <owentemple@gmail.com>
2016-06-23 15:27:11 -0500

    use react router and LinkContainer to send postcard clicks to selected packages

--- client/components/outerCardBox.js
+++ client/components/outerCardBox.js
@@ -4,6 +4,8 @@ import React from 'react';
 import ReactDom from 'react-dom';
 // import CardBox from './cardBox'
 import ImageCarousel from './imageCarousel';
+import { Link } from 'react-router';
+import { LinkContainer } from 'react-router-bootstrap';
 
 class OuterCardBox extends React.Component {
 	render(){
@@ -16,8 +18,8 @@ class OuterCardBox extends React.Component {
 				</div>
 				<div>
 					<div className='center-div row'>
-						<img className='package-uno' src='/assets/images/postcard-package.jpg'></img>
-						<img className='package-dos' src='/assets/images/postcard-package-natural.jpg'></img>
+						<LinkContainer to={{pathname:'/package/Seven Wonders'}}><img className='package-uno' src='/assets/images/postcard-package.jpg'></img></LinkContainer>
+						<LinkContainer to={{pathname:'/package/Seven Natural Wonders'}}><img className='package-dos' src='/assets/images/postcard-package-natural.jpg'></img></LinkContainer>
 					</div>
 				</div>
 				<ImageCarousel />
