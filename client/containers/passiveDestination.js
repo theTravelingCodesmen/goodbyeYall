2803e83915a8049bd05a885e4232c080e90178bc
Paul-Michael Schreiber <paulmichael.schreiber@gmail.com>
2016-06-23 12:43:49 -0500

    makes prices badges

--- client/containers/passiveDestination.js
+++ client/containers/passiveDestination.js
@@ -1,25 +1,26 @@
 'use strict'
-
+​
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Badge } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { setActiveDestination } from '../actions/setActiveDestination';
-
-
+​
+​
 class PassiveDestination extends React.Component {
 	render() {
 		return(	
 			<div className='passive-photo' onClick={()=>this.props.setActiveDestination(this.props._id)} style={{'backgroundImage': 'url(' + this.props.main_image_url + ')'}}>
-				<p className='passive-price'>{"$" + Math.round(this.props.price)}</p>
+				<p><Badge className='passive-price'>{"$" + Math.round(this.props.price)}</Badge></p>
 			</div>
-
+​
 		) 
 	}
 }
-
+​
 function mapDispatchToProps(dispatch) {
 	return bindActionCreators({ setActiveDestination: setActiveDestination }, dispatch);
 }
-
+​
 export default connect(null, mapDispatchToProps)(PassiveDestination);
