a9aff6432b7ce5892e0c59f95ab8807cae31a547
Sin Cheung Ko <scko823@gmail.com>
2016-06-23 18:47:40 -0500

    add comment to setAirport action

--- client/actions/setAirport.js
+++ client/actions/setAirport.js
@@ -4,6 +4,7 @@ export const SET_AIRPORT = 'SET_AIRPORT';
 
 export function setAirport (airport){
 	//airport is the string
+	// console.log('line 7 setAirport.js', airport)
   return {
     type: SET_AIRPORT,
     payload: airport
