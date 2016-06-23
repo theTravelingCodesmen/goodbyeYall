84755347f9cb021e9bb25cd070ba9f15e60e804c
Sin Cheung Ko <scko823@gmail.com>
2016-06-23 18:50:48 -0500

    add airport reducer

--- client/reducers/reducer_airport.js
+++ client/reducers/reducer_airport.js
@@ -6,11 +6,12 @@ import { SET_AIRPORT } from '../actions/setAirport';
 let INITIAL_STATE = { originairport: localStorage.getItem('originairport')|| "HOUA-sky" };
 
 export default function (state = INITIAL_STATE, action) {
-	console.log(state)
+	// console.log('line 9 reducers/reducers_airport.js state is',state);
+	// console.log('line 10 reducers/reducers_airport.js action is',action);
   switch (action.type){
 		case SET_AIRPORT:
     	let newState = {};
-    	newState.originairport = action.payload
+    	newState.originairport = action.payload;
     	return newState
 		default:
 			return state
