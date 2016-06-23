900ba9507bf873df775f738cc69a38819c40730c
Sin Cheung Ko <scko823@gmail.com>
2016-06-23 18:53:23 -0500

    airportDropdown container now dispatch setAirport action when airport is clicked

--- client/containers/airportDropdown.js
+++ client/containers/airportDropdown.js
@@ -3,19 +3,22 @@ import React from 'react';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import {SET_AIRPORT, setAirport} from '../actions/setAirport';
 
 class AirportDropdown extends React.Component {
-  handleSelect(airport) {
-    localStorage.setItem('originairport', airport);
-  }
 	render(){
 		return (
 			<NavDropdown className="navbar-button" eventKey={4} title="Set your airport" id="basic-nav-dropdown">
-        <MenuItem eventKey={4.1} onClick={this.handleSelect.bind(null, "DFWA-sky")}><div className="navbar-button">DFWA-sky</div></MenuItem>
-        <MenuItem eventKey={4.2} onClick={this.handleSelect.bind(null, "HOUA-sky")}><div className="navbar-button">HOUA-sky</div></MenuItem>
+        <MenuItem eventKey={4.1} onClick={this.props.setAirport.bind(null, "DFWA-sky")}><div className="navbar-button">DFWA-sky</div></MenuItem>
+        <MenuItem eventKey={4.2} onClick={this.props.setAirport.bind(null, "HOUA-sky")}><div className="navbar-button">HOUA-sky</div></MenuItem>
       </NavDropdown>
       )
 	}
 }
 
-export default AirportDropdown;
+function mapDispatchToProps( dispatch ){
+  return bindActionCreators({setAirport: setAirport}, dispatch)
+}
+
+
+export default connect(null, mapDispatchToProps)(AirportDropdown);
