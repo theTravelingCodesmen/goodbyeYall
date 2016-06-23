25e222b8e88268f934b2c866e63528434d5b70c7
Paul-Michael Schreiber <paulmichael.schreiber@gmail.com>
2016-06-22 20:08:11 -0500

    wrote out mission statement

--- client/components/missionStatement.js
+++ client/components/missionStatement.js
@@ -8,7 +8,12 @@ class MissionStatement extends React.Component {
 		return (
 				<div className='mission-statement'>
 				<div className='nav-spacing'></div>
-					favorite -> fly -> repeat
+					<h2>How it works</h2>
+					<h4>1. Sign in using Facebook</h4>
+					<h4>2. Favorite our Destination Packages that you want to follow</h4>
+					<h4>3. Get Facebook Notifications when your flights are half off</h4>
+					<h4>4. Discover another Package!</h4>
+					<h4>favorite -> fly -> repeat</h4>
 				</div>
 			)
 	}
