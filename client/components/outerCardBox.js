1683a0c629858d146974b71d714938ebc94b57b6
owentemp <owentemple@gmail.com>
2016-06-23 15:04:02 -0500

    get active destination displaying correctly, front page alignment of elements

--- client/components/outerCardBox.js
+++ client/components/outerCardBox.js
@@ -11,12 +11,12 @@ class OuterCardBox extends React.Component {
 			<div >
 			<div className='nav-spacing'></div>
 				<div className='row'>
-					<img className='value-prop col-md-8' src='/assets/images/value-prop.jpg'></img> 
-				
+					<img className='value-prop col-md-8' src='/assets/images/value-prop.jpg'></img>
+
 				</div>
 				<div>
-					<div className='center-div row'>		
-						<img className='package-uno' src='/assets/images/postcard-package.jpg'></img> 
+					<div className='center-div row'>
+						<img className='package-uno' src='/assets/images/postcard-package.jpg'></img>
 						<img className='package-dos' src='/assets/images/postcard-package-natural.jpg'></img>
 					</div>
 				</div>
