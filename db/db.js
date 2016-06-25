f44009edc3857f2e55333fea2cfe288b9b514e0b
Paul-Michael Schreiber <paulmichael.schreiber@gmail.com>
2016-06-25 15:25:03 -0500

    adds created_at column to users table and cheapest_route_ever table

--- db/db.js
+++ db/db.js
@@ -88,6 +88,13 @@ knex.ensureSchema = function () {
           console.log('Created cheapest_route_ever table.');
         });
       }
+      else if (exists) {
+        knex.schema.table('cheapest_route_ever', function (table) {      
+          table.timestamp('created_at').defaultTo(knex.fn.now());
+        }).then(function (table) {
+          console.log('Altered cheapest_route_ever table.');
+        })
+      }   
     }),
     knex.schema.hasTable('last_thirty_days').then(function (exists) {
       if (!exists) {
@@ -111,37 +118,15 @@ knex.ensureSchema = function () {
           table.string('profile_name', 255);
           table.string('profile_email', 255);
           table.string('profile_photo', 255);          
-        }).then(function (table) {
-          console.log('Created users table.');
-        })
-      }
-      else if (exists) {
-        knex.schema.table('users', function (table) {      
           table.boolean('DFWA-sky').defaultTo(false);
           table.boolean('HOUA-sky').defaultTo(false);
           table.boolean('Seven Wonders').defaultTo(false);
           table.boolean('Seven Natural Wonders').defaultTo(false);
         }).then(function (table) {
-          console.log('Altered users table.');
+          console.log('Created users table.');
         })
-      }          
-    })
-    // knex.schema.hasTable('user_preferences').then(function (exists) {
-    //   if (!exists) {
-    //     knex.schema.createTable('user_preferences', function (table) {
-    //       table.increments('id').primary();
-    //       //fb_id needs to be overwritten when changing user prefs
-    //       table.foreign('fb_id').references('users');
-    //       table.boolean('DFWA-sky').defaultTo(false);
-    //       table.boolean('HOUA-sky').defaultTo(false);
-    //       table.boolean('Seven Wonders').defaultTo(false);
-    //       table.boolean('Seven Natural Wonders').defaultTo(false);
-    //     }).then(function (table) {
-    //       console.log('Created user_preferences table.');
-    //     });
-    //   }
-    // })
-
+      } 
+    })     
   ])
 }
 
