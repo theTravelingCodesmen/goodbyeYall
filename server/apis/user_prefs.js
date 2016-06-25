e40cc912c54a8967dd752109e9a49a663d217c29
Sin Cheung Ko <scko823@gmail.com>
2016-06-25 15:35:56 -0500

    add knex function to insert user preferences into db

--- server/apis/user_prefs.js
+++ server/apis/user_prefs.js
@@ -1,9 +1,16 @@
 'use strict'
 
 let router = require('express').Router();
+let knex = require('../../db/db');
 
 module.exports = router
 
+knex.insertUserPrefs = function(prefsObj) {
+	return knex('users').insert(prefsObj);
+}
+
 router.post('/',function(req, res) {
 	console.log(req.body);
+
 })
+
