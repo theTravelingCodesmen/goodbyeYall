c99987a0e29af2cfe92da2b8c77f31a7a44f558c
Sin Cheung Ko <scko823@gmail.com>
2016-06-25 16:20:01 -0500

    /user_pref now make API call to facebook graph API to fetch user info base on user token

--- server/apis/user_prefs.js
+++ server/apis/user_prefs.js
@@ -2,21 +2,40 @@
 
 let router = require('express').Router();
 let knex = require('../../db/db');
-
+let requestPromise = require('request-promise');
 module.exports = router
 
 knex.insertUserPrefs = function(prefsObj) {
 	return knex('users').insert(prefsObj);
 }
 
+
+
 router.post('/',function(req, res) {
 	console.log(req.body);
-	knex.insertUserPrefs(req.body)
-		.then(function(data){
-			console.log("line 16 after call knex.insertUserPrefs:", data)
-		})
-		.catch(function (err) {
-			console.log("error on line 19",err);
-		})
+	let prefsObj=Object.assign({}, req.body);
+	if (req.body.token){
+		requestPromise(`https://graph.facebook.com/me?fields=id,name,email,picture&access_token=${req.body.token}`)
+			.then((data)=>{
+				data = JSON.parse(data);
+				prefsObj.fb_id = data.id;
+				prefsObj.profile_name = data.name;
+				prefsObj.profile_email = data.email;
+				prefsObj.profile_photo = data.picture.data.url;
+				console.log('line 24 prefsObj before insert into db', prefsObj)
+				return prefsObj
+			})
+			.then(knex.insertUserPrefs)
+			.then(()=>{
+				res.send(prefsObj);
+			});
+	}
+	// knex.insertUserPrefs(req.body)
+	// 	.then(function(data){
+	// 		console.log("line 16 after call knex.insertUserPrefs:", data)
+	// 	})
+	// 	.catch(function (err) {
+	// 		console.log("error on line 19",err);
+	// 	})
 })
 
