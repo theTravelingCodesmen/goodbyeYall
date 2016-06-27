caa88e050982cd6efab4db62420fe70a58ad75a0
Paul-Michael Schreiber <paulmichael.schreiber@gmail.com>
2016-06-27 16:48:15 -0500

    changes way date is displayed and adds outboud city

--- client/components/activeDestination.js
+++ client/components/activeDestination.js
@@ -5,6 +5,8 @@ import ReactDOM from 'react-dom';
 import D3CacheGraph from './d3CacheGraph';
 import FlightData from './flightData';
 import { connect } from 'react-redux';
+import numberToMonth from '../helper/number_to_month';
+import airportToCity from '../helper/airport_to_city';
 
 
 class ActiveDestination extends React.Component {
@@ -19,7 +21,7 @@ class ActiveDestination extends React.Component {
 				<div className='flight-info-container'>
 					<FlightData airport_code={this.props.airport_code}/>
 					<div className='flight-details'>
-						<p className='dates'>{'Leaving on ' + this.props.bookingDetails.outboundDate.slice(0,10) + ' and returning on ' + this.props.bookingDetails.inboundDate.slice(0,10)}</p>
+						<p className='dates'>{'Leaving ' + airportToCity[localStorage.getItem('originairport')] + ' on ' + numberToMonth[this.props.bookingDetails.outboundDate.slice(5,7)] + ' ' + this.props.bookingDetails.outboundDate.slice(8,10) + ' and returning on ' + numberToMonth[this.props.bookingDetails.inboundDate.slice(5,7)] + ' ' + this.props.bookingDetails.inboundDate.slice(8,10)}</p>
 						<h3 className='active-price'>{this.props.bookingDetails.price.toFixed(2)}</h3>
 						<p className='time-ago'>{Math.round((Date.now() - new Date(this.props.bookingDetails.created_at))/(60*60*1000)) + ' hours ago'}</p>
 						<a className='btn btn-primary' href={this.props.bookingDetails.deepLink} target='_blank'>BUY NOW</a>
