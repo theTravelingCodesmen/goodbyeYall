6e11d866314a22fb647f5354896ef9a6fb7d3a34
owentemp <owentemple@gmail.com>
2016-06-28 14:55:25 -0500

    add travel packages to front page

--- client/components/outerCardBox.js
+++ client/components/outerCardBox.js
@@ -22,6 +22,10 @@ class OuterCardBox extends React.Component {
 						<LinkContainer to={{pathname:'/package/Seven Wonders'}}><img className='package-uno' src='/assets/images/postcard-package.jpg'></img></LinkContainer>
 						<LinkContainer to={{pathname:'/package/Seven Natural Wonders'}}><img className='package-dos' src='/assets/images/postcard-package-natural.jpg'></img></LinkContainer>
 					</div>
+					<div className='center-div row'>
+						<LinkContainer to={{pathname:'/package/American Cities'}}><img className='package-dos' src='/assets/images/postcard-package-4.jpg'></img></LinkContainer>
+						<LinkContainer to={{pathname:'/package/Global Explorer'}}><img className='package-uno' src='/assets/images/postcard-package-3.jpg'></img></LinkContainer>
+					</div>
 				</div>
 				<ImageCarousel />
 			</div>
