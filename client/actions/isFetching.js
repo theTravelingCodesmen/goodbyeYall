ffcdd20951a98b2ef5504fbff33097d8785fc0a4
Sin Cheung Ko <scko823@gmail.com>
2016-06-28 19:54:45 -0500

    package state now fill fetching property to true when the package is fetched

--- client/actions/isFetching.js
+++ client/actions/isFetching.js
@@ -4,7 +4,6 @@
 export const IS_FETCHING ='IS_FETCHING';
 
 export function changeFetching(bool){
-	console.log('line 7 isFetching running with input',bool)
 	return {
 		type: IS_FETCHING,
 		payload: bool
