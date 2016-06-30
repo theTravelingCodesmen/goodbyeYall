76f4aef8db2443daa17511780a7656977ebdc9dc
Sin Cheung Ko <scko823@gmail.com>
2016-06-30 11:47:20 -0500

    remove current airport feature

--- client/containers/airportDropdown.js
+++ client/containers/airportDropdown.js
@@ -5,14 +5,12 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import {SET_AIRPORT, setAirport} from '../actions/setAirport';
 
-letairport = localStorage.getItem('originairport');
-
 class AirportDropdown extends React.Component {
 	render(){
 		return (
 			<NavDropdown className="navbar-button" eventKey={4} title="Set your airport" id="basic-nav-dropdown">
-        <MenuItem eventKey={4.1} onClick={this.props.setAirport.bind(null, "DFWA-sky")}><div className="navbar-button" className= {airport === "DFWA-sky" ? "current":"" } >Dallas/Fort Worth</div></MenuItem>
-        <MenuItem eventKey={4.2} onClick={this.props.setAirport.bind(null, "HOUA-sky")}><div className="navbar-button" className= {airport === "HOUA-sky" ? "current":"" }>Houston</div></MenuItem>
+        <MenuItem eventKey={4.1} onClick={this.props.setAirport.bind(null, "DFWA-sky")}><div className="navbar-button">Dallas/Fort Worth</div></MenuItem>
+        <MenuItem eventKey={4.2} onClick={this.props.setAirport.bind(null, "HOUA-sky")}><div className="navbar-button">Houston</div></MenuItem>
       </NavDropdown>
       )
 	}
