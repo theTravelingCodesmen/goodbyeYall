b0c457418128d47ac0eef73d7d8b84b1172d5d9b
Sin Cheung Ko <scko823@gmail.com>
2016-07-01 14:17:02 -0500

    cleanup airport reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,6 +3,9 @@ import Destination from './reducer_destination'
 import Airport from './reducer_airport'
 import isLoggedIn from './reducer_login.js'
 
+/**
+ * combine reducers to rootReducer
+ */
 const rootReducer = combineReducers({
 	destinations: Destination,
 	airport: Airport,
