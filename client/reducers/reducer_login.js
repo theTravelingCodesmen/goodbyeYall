38f451e4704bb31dae101de7c71288a3422759a8
Sin Cheung Ko <scko823@gmail.com>
2016-07-01 14:22:07 -0500

    clean up login reducer

--- client/reducers/reducer_login.js
+++ client/reducers/reducer_login.js
@@ -3,6 +3,12 @@ import { IS_LOGGED_IN } from '../actions/isLoggedIn';
 
 let INITIAL_STATE = { isLoggedIn: !!localStorage.getItem('goodbyeyall.fb_id') };
 
+
+/**
+ * airport reducer that change set the state of isLoggedIn
+ * @param {object} action - { type: {string}, paylaod : {object} }
+ */
+
 export default function (state = INITIAL_STATE, action) {
   switch (action.type){
 		case IS_LOGGED_IN:
