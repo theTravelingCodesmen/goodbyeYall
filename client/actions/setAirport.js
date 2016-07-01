4928cb69d274a5385efed2c43a80277c336450a3
Sin Cheung Ko <scko823@gmail.com>
2016-07-01 09:46:47 -0500

    clean up setAirport action

--- client/actions/setAirport.js
+++ client/actions/setAirport.js
@@ -2,9 +2,13 @@
 
 export const SET_AIRPORT = 'SET_AIRPORT'; 
 
+/**
+ * Action fired to when a user set origin airport
+ * @param {bool} airport - is origin airport selected by userg
+ */
+
 export function setAirport (airport){
-	//airport is the string
-	// console.log('line 7 setAirport.js', airport)
+	// console.log('line 12 setAirport.js', airport)
   return {
     type: SET_AIRPORT,
     payload: airport
