6aeb3cf81cdd429f830b39475a57b317f9957900
James <Fitzi9r@gmail.com>
2016-07-01 16:28:55 -0500

    preparing for enzyme test

---
+++ test/UI/enzTest.js
@@ -0,0 +1,62 @@
+'use strict'
+
+// import React from 'react';
+// import { shallow, mount} from 'enzyme';
+
+// import D3CacheGraph from '../../components/d3CacheGraph';
+// import FlightData from '../../components/flightData';
+// import BootStrapModal from '../../components/dynamicModals';
+// import getd3Cachegraph from '../../components/d3CacheGraphFetch';
+// import ImageCarousel from '../../components/imageCarousel';
+// import CardTitle from '../../components/cardTitle';
+// import PackageDestinations from '../../components/packageDestinations';
+// import PassiveDestination from '../../components/passiveDestination';
+// import ActiveDestination from '../../components/activeDestination';
+
+// import NavBar from '../../containers/navBar';
+// import OuterCardBox from '../../components/outerCardBox';
+// import MissionStatement from '../../components/missionStatement';
+// import MeetTheDevs from '../../components/meetTheDevs';
+// import CardBox from '../../containers/cardBox';
+// import UserForm from '../../components/userForm';
+// import AirportDropdown from '../../containers/airportDropdown';
+// import {expect} from 'chai';
+let expect = require('chai').expect;
+// Shallow Rendering
+// Full Dom Rendering
+// Static Rendered Markup
+
+
+
+// describe('',() =>{
+// 	it('message',() =>{
+// 		expect(shallow().contains()).to.equal(true);
+// 	});
+// 	it('message',() =>{
+// 		expect(shallow().is())to.equal(true);
+// 	});
+// 	it('message',() =>{
+// 		expect(mount().find().length).to.equal();
+// 	});
+// 	it('calls componentDidMount', () =>{
+// 		const wrapper = mount(xxxxx);
+// 		expect(xxxxx.prototype.componentDidMount.calledOnce).to.equal(true);
+// 	})
+// })
+
+
+describe('<NavBar />',() =>{
+	it('it should render <OuterCardBox /> component',() =>{
+		expect(true).to.equal(true);
+	});
+	// it('message',() =>{
+	// 	expect(shallow(<NavBar />).find((<MissionStatement />)to.have.length(1);
+	// });
+	// it('message',() =>{
+	// 	expect(mount().find().length).to.equal();
+	// });
+	// it('calls componentDidMount', () =>{
+	// 	const wrapper = mount(xxxxx);
+	// 	expect(xxxxx.prototype.componentDidMount.calledOnce).to.equal(true);
+	// })
+})
