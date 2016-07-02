58c8d71080c26039a9a758b600a94882d0e50cfe
Paul-Michael <paulmichael.schreiber@gmail.com>
2016-07-02 12:18:06 -0500

    changes call to action color

--- client/components/outerCardBox.js
+++ client/components/outerCardBox.js
@@ -13,7 +13,7 @@ class OuterCardBox extends React.Component {
 				<div className='value-prop'>
 					<h1 className='main'>We do the work, you do the travel</h1>
 					<h2 className='sub'>Thousands of flights. One low price.</h2>
-					<h5 className='call-to-action'>Choose a travel theme or <a href="http://www.facebook.com/dialog/oauth?client_id=1071311906250508&scope=email&response_type=token&redirect_uri=http://localhost:4000/Preferences">log in</a> to get started</h5>
+					<h5 className='call-to-action'>Choose a travel theme or <a className="call-to-action-link" href="http://www.facebook.com/dialog/oauth?client_id=1071311906250508&scope=email&response_type=token&redirect_uri=http://localhost:4000/Preferences">log in</a> to get started</h5>
 				</div>
 				<div>
 					<div className='center-div row'>
