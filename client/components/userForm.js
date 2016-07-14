3d77a7aa38f496f4aac3b5da99b82aeba593b485
Paul-Michael <paulmichael.schreiber@gmail.com>
2016-07-13 19:00:46 -0500

    adds cities to the rest of the packages

--- client/components/userForm.js
+++ client/components/userForm.js
@@ -114,14 +114,16 @@ let UserForm = React.createClass ({
             Global Explorer - Bangkok, Dubai, Istanbul, London, Paris, Singapore & Seoul
           </Checkbox>
           <Checkbox checked={this.state['Party Islands']} onChange={()=>{this.setState({'Party Islands':!this.state['Party Islands']});}}>
-            Party Islands
+            Party Islands - Aruba, Fiji, Grand Cayman Island, Ibiza, Ko Phi Phi, Maui & Mykonos
           </Checkbox> 
           <Checkbox checked={this.state['Seven Wonders']} onChange={()=>{this.setState({'Seven Wonders':!this.state['Seven Wonders']});}}>
-            Seven Wonders of the World
+            Seven Wonders of the World - Chichén Itzá, Christ the Redeemer, Colosseum, Machu Picchu, Petra, Taj Mahal & The Great Wall
           </Checkbox>
           <Checkbox checked={this.state['Seven Natural Wonders']} onChange={()=>{this.setState({'Seven Natural Wonders':!this.state['Seven Natural Wonders']});}}>
-            Seven Natural Wonders of the World
+            Seven Natural Wonders of the World - Aurora Borealis, Great Barrier Reef, Harbor of Rio de Janeiro, Mount Everest, Parícutin Volcano, The Grand Canyon & Victoria Falls
           </Checkbox> 
+          <b>Click submit to save your preferences and continue your adventure.</b>
+          <br/>
           <Button className="prefs-submit-button btn" type="submit">
             Submit
           </Button>
